16811f4bddf48c6b5570c8b4aab67a3489d0a2f0
Manvendra Singh Bais <47491368+Manvendra5@users.noreply.github.com>
2021-09-14 16:44:59 +0530

    Play Function

--- index.js
+++ index.js
@@ -26,3 +26,49 @@ var highScores = [
         score: 2,
     },
 ];
+
+//Play funtion: This function will start the quiz
+function play(questions) {
+    var userName = readlineSync.question("What's your name? ");
+    console.log("Welcome " + userName + " to this quiz on Manvendra");
+    console.log("------------------------------------------------------");
+
+    for (var i = 0; i < questions.length; i += 1) {
+        var currentQuestion = questions[i];
+        var userAnswer = readlineSync.question(currentQuestion["question"]);
+
+        if (userAnswer.toUpperCase() === currentQuestion.answer.toUpperCase()) {
+            score = score + 1;
+            console.log("You are correct!");
+            console.log("Your Current Score is ", score);
+        }
+        else {
+            if (score) {
+                score = score - 1;
+            }
+            console.log("You are wrong!");
+            console.log("Your Current Score is ", score);
+        }
+    }
+
+    console.log("------------------------------------------------------");
+
+    console.log(`
+Your Final Score is ${score}
+Top Standings:
+`
+    );
+    for (var i = 0; i < highScores.length; i += 1) {
+        var scorer = highScores[i];
+        console.log(scorer.name + " : " + scorer.score);
+    }
+
+    console.log("If you think your name should be in that list, ping me I will update the High Scores.");
+
+    console.log(`
+Thanks for playing. 
+Refresh the page to play again.
+For suggestions to improve this quiz, ping me!`);
+};
+
+play(questions);
